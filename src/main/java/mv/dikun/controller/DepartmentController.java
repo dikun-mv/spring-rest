79d922f779eb816760eecee1e86746586c24d202
Maxim Dikun <dikun.mv@gmail.com>
2016-11-30 22:58:42 +0300

    Enable CORS

--- src/main/java/mv/dikun/controller/DepartmentController.java
+++ src/main/java/mv/dikun/controller/DepartmentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DepartmentController {
